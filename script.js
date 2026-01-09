9610ffcb3e8ef9ad980bf0eb714497fd38029aec
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-01-10 00:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 800; i++) {
+  const square = document.createElement("span"); // new element each loop
+  square.className = "square";
+  container.appendChild(square);
+}
